30f4a49a56f67b08e8a39e568dd8162d686a3878
yondikavl <yondikaviolanda@gmail.com>
2024-06-09 09:59:36 +0700

    fix: home & history

--- app/src/main/java/com/bangkit/tanamify/data/local/TanamifyDatabase.kt
+++ app/src/main/java/com/bangkit/tanamify/data/local/TanamifyDatabase.kt
@@ -6,22 +6,22 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [HistoryEntity::class], version = 1)
-abstract class AsclepiusDatabase: RoomDatabase() {
+abstract class TanamifyDatabase: RoomDatabase() {
     abstract fun historyDao(): HistoryDao
     companion object{
         @Volatile
-        private var INSTANCE: AsclepiusDatabase? = null
+        private var INSTANCE: TanamifyDatabase? = null
         @JvmStatic
-        fun getDatabase(context: Context):AsclepiusDatabase{
+        fun getDatabase(context: Context):TanamifyDatabase{
             if (INSTANCE == null){
-                synchronized(AsclepiusDatabase::class.java){
+                synchronized(TanamifyDatabase::class.java){
                     INSTANCE = Room.databaseBuilder(context.applicationContext,
-                        AsclepiusDatabase::class.java, "asclepius_database")
+                        TanamifyDatabase::class.java, "tanamify_database")
                         .allowMainThreadQueries()
                         .build()
                 }
             }
-            return INSTANCE as AsclepiusDatabase
+            return INSTANCE as TanamifyDatabase
         }
     }
 }
